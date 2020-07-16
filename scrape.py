032be63815cc6c211cb9e8cabc10c04c98eefc09
azikuma <42406254+azikuma@users.noreply.github.com>
2020-07-16 13:02:23 +0900

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,31 @@
+import requests, os, time
+from bs4 import BeautifulSoup
+from urllib import parse
+
+os.makedirs('images', exist_ok=True)
+url = 'https://sai-zen-sen.jp/comics/twi4/otomedanshi/'
+
+htmls = []
+
+soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+comics = soup.select_one('section#comics')
+
+for link in comics.select('a'):
+    if link.get('target') == '_blank':
+        continue
+    else:
+        htmls.append(link.get('href'))
+
+htmls.reverse()
+
+base_url = 'https://sai-zen-sen.jp'
+for html in range(len(htmls)):
+    html_url = htmls[html]
+    soup = BeautifulSoup(requests.get(html_url).content, 'html.parser')
+    src = soup.select_one('p > img').get('src')
+    img = base_url + src
+    num_name = (str)(html + 1) + '.jpg'
+
+    print(img)
+
+print('Done!')
